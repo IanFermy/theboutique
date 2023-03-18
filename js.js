1443241fe12377524dc46730a8f7509d3477e932
Ian Fermy <greenartistenoir.buster.if@gmail.com>
2023-03-18 15:35:31 -0400

    updated index page

---
+++ js.js
@@ -0,0 +1,19 @@
+var hidden = document.getElementById('hidden_div')
+var hiddenTwo = document.getElementById('hidden_divTwo')
+var hiddenThree = document.getElementById('hidden_divThree')
+
+
+function show() {
+    console.log(hidden)
+    hidden.style.display = "block"
+}
+
+function showTwo() {
+    console.log(hidden)
+    hiddenTwo.style.display = "block"
+}
+
+function showThree() {
+    console.log(hidden)
+    hiddenThree.style.display = "block"
+}
